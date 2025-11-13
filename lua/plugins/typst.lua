893c68435915930c0997f9755a164f63b5867640
Lukas Werner <lukas.m.werner@outlook.com>
2025-11-12 23:49:16 -0800

    omg mini.ai is great. fixes a and i commands, make tinymist only load on typst files

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -1,10 +1,16 @@
 return {
 	{
 		'chomosuke/typst-preview.nvim',
-		lazy = false, -- or ft = 'typst'
+		ft = 'typst',
 		version = '1.*',
 		config = function()
 			require 'typst-preview'.setup {}
 		end,
-	}
+	},
+	{
+		'nvim-mini/mini.ai',
+		version = false,
+		ft = 'typst',
+		config = {},
+	},
 }
